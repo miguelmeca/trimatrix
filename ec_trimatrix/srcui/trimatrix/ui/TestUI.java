7ea93a1948a4e7a26e61dc20fbeab89a6b49e235
reich.markus <reich.markus@2258e520-3029-11de-a0ce-9586e7784243>
2009-06-22 21:11:26 +0000

    Corrected TestUI PDF generation

--- ec_trimatrix/srcui/trimatrix/ui/TestUI.java
+++ ec_trimatrix/srcui/trimatrix/ui/TestUI.java
@@ -28,8 +28,6 @@ import org.jfree.data.xy.XYSeries;
 import org.jfree.data.xy.XYSeriesCollection;
 import org.jfree.ui.TextAnchor;
 
-import trimatrix.exceptions.ArrayNotEvenException;
-import trimatrix.exceptions.OutOfBoundsException;
 import trimatrix.structures.SAttachment;
 import trimatrix.ui.utils.MyWorkpageDispatchedBean;
 import trimatrix.utils.Constants;
@@ -42,6 +40,7 @@ import com.lowagie.text.Element;
 import com.lowagie.text.Paragraph;
 import com.lowagie.text.Rectangle;
 import com.lowagie.text.pdf.BaseField;
+import com.lowagie.text.pdf.PdfAction;
 import com.lowagie.text.pdf.PdfAnnotation;
 import com.lowagie.text.pdf.PdfBorderDictionary;
 import com.lowagie.text.pdf.PdfContentByte;
@@ -50,6 +49,7 @@ import com.lowagie.text.pdf.PdfPCell;
 import com.lowagie.text.pdf.PdfPCellEvent;
 import com.lowagie.text.pdf.PdfPTable;
 import com.lowagie.text.pdf.PdfWriter;
+import com.lowagie.text.pdf.PushbuttonField;
 import com.lowagie.text.pdf.TextField;
 
 @SuppressWarnings("serial")
@@ -334,7 +334,7 @@ public class TestUI extends MyWorkpageDispatchedBean implements Serializable
 			// speed	
 			table.addCell("Geschwindigkeit"); 
 			TextField speedField = new TextField(writer, 
-				new Rectangle(0,0,150,10), "speed");
+				new Rectangle(0,0,100,10), "speed");
 			speedField.setBackgroundColor(Color.WHITE);
 			speedField.setBorderColor(Color.BLACK);
 			speedField.setBorderWidth(1);
@@ -347,10 +347,14 @@ public class TestUI extends MyWorkpageDispatchedBean implements Serializable
 			cell.setCellEvent(new FieldCell(speedField.getTextField(), 300, writer));
 			table.addCell(cell);
 			
+			// add empty row
+			table.addCell("");
+			table.addCell("");
+			
 			// heart rate
 			table.addCell("Herzfrequenz"); 
 			TextField hrField = new TextField(writer, 
-				new Rectangle(0,0,150,10), "hr");
+				new Rectangle(0,0,100,10), "hr");
 			hrField.setBackgroundColor(Color.WHITE);
 			hrField.setBorderColor(Color.BLACK);
 			hrField.setBorderWidth(1);
@@ -363,10 +367,14 @@ public class TestUI extends MyWorkpageDispatchedBean implements Serializable
 			cell.setCellEvent(new FieldCell(hrField.getTextField(), 300, writer));
 			table.addCell(cell);
 			
+			// add empty row
+			table.addCell("");
+			table.addCell("");
+			
 			// lactate
 			table.addCell("Laktat"); 
 			TextField lacField = new TextField(writer, 
-				new Rectangle(0,0,150,10), "hr");
+				new Rectangle(0,0,100,10), "lactate");
 			lacField.setBackgroundColor(Color.WHITE);
 			lacField.setBorderColor(Color.BLACK);
 			lacField.setBorderWidth(1);
@@ -379,6 +387,81 @@ public class TestUI extends MyWorkpageDispatchedBean implements Serializable
 			cell.setCellEvent(new FieldCell(lacField.getTextField(), 300, writer));
 			table.addCell(cell);
 			
+			// add empty row
+			table.addCell("");
+			table.addCell("");
+			
+			// add commet textfield
+			table.addCell("Kommentar");
+			TextField comment = new TextField(writer, 
+				new Rectangle(0, 0,100,	100), "comment");
+			comment.setBorderColor(Color.BLACK);
+			comment.setBorderWidth(1);
+			comment.setBorderStyle(PdfBorderDictionary.STYLE_SOLID);
+			comment.setText("");
+			comment.setOptions(BaseField.MULTILINE | 
+				BaseField.DO_NOT_SCROLL);
+			cell = new PdfPCell();
+			cell.setMinimumHeight(100);
+			cell.setCellEvent(new FieldCell(comment.getTextField(), 
+				300, writer));
+			table.addCell(cell);
+			
+			
+			// check comment characters length javascript
+			writer.addJavaScript(
+				"var commentField = " +
+				"this.getField('commentField');" +
+				"commentField" +
+				".setAction('Keystroke','checkLength()');" +
+				"function checkLength(){" +
+				"if(!event.willCommit && " +
+				"event.value.length > 100){" +
+				"app.alert('Warning! Comment can not " +
+				"be more than 100 characters.');" +
+				"event.change = '';" +
+				"}}");			
+			
+			
+			// submit button	
+			PushbuttonField submitBtn = new PushbuttonField(writer,
+					new Rectangle(0, 0, 35, 15),"submitPOST");
+			submitBtn.setBackgroundColor(Color.GRAY);
+			submitBtn.
+				setBorderStyle(PdfBorderDictionary.STYLE_BEVELED);
+			submitBtn.setText("POST");
+			submitBtn.setOptions(BaseField.
+				VISIBLE_BUT_DOES_NOT_PRINT);
+			PdfFormField submitField = submitBtn.getField();
+			submitField.setAction(PdfAction
+			.createSubmitForm(
+			"http://localhost:50000/trimatrix/servlet/AcroForm",
+			null, PdfAction.SUBMIT_HTML_FORMAT));
+			//submitField.setAction(PdfAction.javaScript("app.alert(this.submitForm('http://localhost:50000/trimatrix/servlet/AcroForm'))", writer));
+					
+			cell = new PdfPCell();
+			cell.setMinimumHeight(15);
+			cell.setCellEvent(new FieldCell(submitField, 35, writer));
+			table.addCell(cell);
+					
+			// reset button
+			PushbuttonField resetBtn = new PushbuttonField(writer,
+					new Rectangle(0, 0, 35, 15), "reset");
+			resetBtn.setBackgroundColor(Color.GRAY);
+			resetBtn.setBorderStyle(
+				PdfBorderDictionary.STYLE_BEVELED);
+			resetBtn.setText("RESET");
+			resetBtn
+			.setOptions(
+				BaseField.VISIBLE_BUT_DOES_NOT_PRINT);
+			PdfFormField resetField = resetBtn.getField();
+			resetField.setAction(PdfAction.createResetForm(null, 0));
+			cell = new PdfPCell();
+			cell.setMinimumHeight(15);
+			cell.setCellEvent(new FieldCell(resetField, 35, writer));
+			table.addCell(cell);		
+					
+			document.add(table);
 			document.close();			
 		} catch (Exception ex) {	
 			Statusbar.outputError("Error creating PDF",ex.toString());
