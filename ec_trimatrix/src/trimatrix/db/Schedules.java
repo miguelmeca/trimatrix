0aafa17dfbef72bc7f6f60ebf74f2cad1e02cea8
reich.markus <reich.markus@gmail.com>
2010-01-26 22:37:03 +0000

    Added schedule db tables

---
+++ ec_trimatrix/src/trimatrix/db/Schedules.java
@@ -0,0 +1,182 @@
+package trimatrix.db;
+
+import java.sql.Timestamp;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import trimatrix.entities.IEntityObject;
+
+/**
+ * Schedules entity. @author MyEclipse Persistence Tools
+ */
+@Entity
+@Table(name = "schedules", catalog = "trimatrix")
+public class Schedules implements java.io.Serializable, IEntityObject {
+
+	// Fields
+
+	private String id;
+	private String personId;
+	private String type;
+	private String description;
+	private Timestamp start;
+	private Integer duration;
+	private String color;
+	private Timestamp createdAt;
+	private String createdBy;
+	private Timestamp modifiedAt;
+	private String modifiedBy;
+	private Boolean deleted;
+	private Boolean test;
+
+	// Constructors
+
+	/** default constructor */
+	public Schedules() {
+	}
+
+	/** minimal constructor */
+	public Schedules(String id) {
+		this.id = id;
+	}
+
+	/** full constructor */
+	public Schedules(String id, String personId, String type, String description, Timestamp start, Integer duration, String color, Timestamp createdAt, String createdBy, Timestamp modifiedAt,
+			String modifiedBy, Boolean deleted, Boolean test) {
+		this.id = id;
+		this.personId = personId;
+		this.type = type;
+		this.description = description;
+		this.start = start;
+		this.duration = duration;
+		this.color = color;
+		this.createdAt = createdAt;
+		this.createdBy = createdBy;
+		this.modifiedAt = modifiedAt;
+		this.modifiedBy = modifiedBy;
+		this.deleted = deleted;
+		this.test = test;
+	}
+
+	// Property accessors
+	@Id
+	@Column(name = "id", unique = true, nullable = false, length = 36)
+	public String getId() {
+		return this.id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	@Column(name = "person_id", length = 36)
+	public String getPersonId() {
+		return this.personId;
+	}
+
+	public void setPersonId(String personId) {
+		this.personId = personId;
+	}
+
+	@Column(name = "type", length = 10)
+	public String getType() {
+		return this.type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
+	@Column(name = "description", length = 65535)
+	public String getDescription() {
+		return this.description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	@Column(name = "start", length = 19)
+	public Timestamp getStart() {
+		return this.start;
+	}
+
+	public void setStart(Timestamp start) {
+		this.start = start;
+	}
+
+	@Column(name = "duration")
+	public Integer getDuration() {
+		return this.duration;
+	}
+
+	public void setDuration(Integer duration) {
+		this.duration = duration;
+	}
+
+	@Column(name = "color", length = 10)
+	public String getColor() {
+		return this.color;
+	}
+
+	public void setColor(String color) {
+		this.color = color;
+	}
+
+	@Column(name = "created_at", length = 19)
+	public Timestamp getCreatedAt() {
+		return this.createdAt;
+	}
+
+	public void setCreatedAt(Timestamp createdAt) {
+		this.createdAt = createdAt;
+	}
+
+	@Column(name = "created_by", length = 36)
+	public String getCreatedBy() {
+		return this.createdBy;
+	}
+
+	public void setCreatedBy(String createdBy) {
+		this.createdBy = createdBy;
+	}
+
+	@Column(name = "modified_at", length = 19)
+	public Timestamp getModifiedAt() {
+		return this.modifiedAt;
+	}
+
+	public void setModifiedAt(Timestamp modifiedAt) {
+		this.modifiedAt = modifiedAt;
+	}
+
+	@Column(name = "modified_by", length = 36)
+	public String getModifiedBy() {
+		return this.modifiedBy;
+	}
+
+	public void setModifiedBy(String modifiedBy) {
+		this.modifiedBy = modifiedBy;
+	}
+
+	@Column(name = "deleted")
+	public Boolean getDeleted() {
+		return this.deleted;
+	}
+
+	public void setDeleted(Boolean deleted) {
+		this.deleted = deleted;
+	}
+
+	@Column(name = "test")
+	public Boolean getTest() {
+		return this.test;
+	}
+
+	public void setTest(Boolean test) {
+		this.test = test;
+	}
+
+}
---
+++ ec_trimatrix/src/trimatrix/db/TScheduletypesDAO.java
@@ -0,0 +1,141 @@
+package trimatrix.db;
+
+import java.util.List;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.hibernate.LockMode;
+import org.springframework.context.ApplicationContext;
+import org.springframework.orm.hibernate3.support.HibernateDaoSupport;
+
+/**
+ * A data access object (DAO) providing persistence and search support for
+ * TScheduletypes entities. Transaction control of the save(), update() and
+ * delete() operations can directly support Spring container-managed
+ * transactions or they can be augmented to handle user-managed Spring
+ * transactions. Each of these methods provides additional information for how
+ * to configure it for the desired type of transaction control.
+ * 
+ * @see trimatrix.db.TScheduletypes
+ * @author MyEclipse Persistence Tools
+ */
+
+public class TScheduletypesDAO extends HibernateDaoSupport implements ITextDAO<TScheduletypes, TScheduletypesId> {
+	private static final Log log = LogFactory.getLog(TScheduletypesDAO.class);
+
+	protected void initDao() {
+		// do nothing
+	}
+
+	public void save(TScheduletypes transientInstance) {
+		log.debug("saving TScheduletypes instance");
+		try {
+			getHibernateTemplate().save(transientInstance);
+			log.debug("save successful");
+		} catch (RuntimeException re) {
+			log.error("save failed", re);
+			throw re;
+		}
+	}
+
+	public void delete(TScheduletypes persistentInstance) {
+		log.debug("deleting TScheduletypes instance");
+		try {
+			getHibernateTemplate().delete(persistentInstance);
+			log.debug("delete successful");
+		} catch (RuntimeException re) {
+			log.error("delete failed", re);
+			throw re;
+		}
+	}
+
+	public TScheduletypes findById(trimatrix.db.TScheduletypesId id) {
+		log.debug("getting TScheduletypes instance with id: " + id);
+		try {
+			TScheduletypes instance = (TScheduletypes) getHibernateTemplate().get("trimatrix.db.TScheduletypes", id);
+			return instance;
+		} catch (RuntimeException re) {
+			log.error("get failed", re);
+			throw re;
+		}
+	}
+
+	@SuppressWarnings("unchecked")
+	public List<TScheduletypes> findByExample(TScheduletypes instance) {
+		log.debug("finding TScheduletypes instance by example");
+		try {
+			List<TScheduletypes> results = (List<TScheduletypes>) getHibernateTemplate().findByExample(instance);
+			log.debug("find by example successful, result size: " + results.size());
+			return results;
+		} catch (RuntimeException re) {
+			log.error("find by example failed", re);
+			throw re;
+		}
+	}
+
+	@SuppressWarnings("unchecked")
+	public List<TScheduletypes> findByProperty(String propertyName, Object value) {
+		log.debug("finding TScheduletypes instance with property: " + propertyName + ", value: " + value);
+		try {
+			String queryString = "from TScheduletypes as model where model." + propertyName + "= ?";
+			return getHibernateTemplate().find(queryString, value);
+		} catch (RuntimeException re) {
+			log.error("find by property name failed", re);
+			throw re;
+		}
+	}
+
+	@SuppressWarnings("unchecked")
+	public List<TScheduletypes> findAll() {
+		log.debug("finding all TScheduletypes instances");
+		try {
+			String queryString = "from TScheduletypes";
+			return getHibernateTemplate().find(queryString);
+		} catch (RuntimeException re) {
+			log.error("find all failed", re);
+			throw re;
+		}
+	}
+
+	public TScheduletypes merge(TScheduletypes detachedInstance) {
+		log.debug("merging TScheduletypes instance");
+		try {
+			TScheduletypes result = (TScheduletypes) getHibernateTemplate().merge(detachedInstance);
+			log.debug("merge successful");
+			return result;
+		} catch (RuntimeException re) {
+			log.error("merge failed", re);
+			throw re;
+		}
+	}
+
+	public void attachDirty(TScheduletypes instance) {
+		log.debug("attaching dirty TScheduletypes instance");
+		try {
+			getHibernateTemplate().saveOrUpdate(instance);
+			log.debug("attach successful");
+		} catch (RuntimeException re) {
+			log.error("attach failed", re);
+			throw re;
+		}
+	}
+
+	public void attachClean(TScheduletypes instance) {
+		log.debug("attaching clean TScheduletypes instance");
+		try {
+			getHibernateTemplate().lock(instance, LockMode.NONE);
+			log.debug("attach successful");
+		} catch (RuntimeException re) {
+			log.error("attach failed", re);
+			throw re;
+		}
+	}
+	
+	@Override
+	public TScheduletypes findById(String key, String languageKey) {
+		return findById(new TScheduletypesId(key, languageKey));
+	}
+
+	public static TScheduletypesDAO getFromApplicationContext(ApplicationContext ctx) {
+		return (TScheduletypesDAO) ctx.getBean("TScheduletypesDAO");
+	}	
+}
