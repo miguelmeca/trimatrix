cd80bb476fd332c4cc6002fd1298e39dd808b5bf
reich.markus@gmail.com <reich.markus@gmail.com@2258e520-3029-11de-a0ce-9586e7784243>
2010-03-17 22:27:19 +0000

    Import results

--- trunk/ec_trimatrix/src/trimatrix/logic/ImportLogic.java
+++ trunk/ec_trimatrix/src/trimatrix/logic/ImportLogic.java
@@ -1,9 +1,11 @@
 package trimatrix.logic;
 
+import java.util.Arrays;
 import java.util.List;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.eclnt.jsfserver.defaultscreens.Statusbar;
 
 import trimatrix.db.DAOLayer;
 import trimatrix.db.ImportTemplates;
@@ -14,7 +16,7 @@ public class ImportLogic {
 	public static final Log logger = LogFactory.getLog(ImportLogic.class);
 	private DAOLayer daoLayer;
 	private ServiceLayer serviceLayer;
-	
+
 	public List<ImportTemplates> getMyTemplates(String entity) {
 		ImportTemplatesId id = new ImportTemplatesId();
 		id.setEntity(entity);
@@ -22,25 +24,27 @@ public class ImportLogic {
 		ImportTemplates example = new ImportTemplates(id);
 		return daoLayer.getImportTemplatesDAO().findByExample(example);
 	}
-	
-	public boolean saveTemplate(String entity, String description, int startingRow, String mapping) {
+
+	public boolean saveTemplate(String entity, String description, int startingRow, int[] mapping) {
 		try {
+			String strMapping = Arrays.toString(mapping);
 			ImportTemplatesId id = new ImportTemplatesId(entity, serviceLayer.getDictionaryService().getMyPerson().getId(), description);
 			ImportTemplates template = new ImportTemplates(id);
 			template.setStartingRow(startingRow);
-			template.setMapping(mapping);
+			template.setMapping(strMapping);
 			daoLayer.getImportTemplatesDAO().save(template);
+			Statusbar.outputSuccess("Template saved!");
 			return true;
 		} catch (Exception ex) {
-			//Statusbar.outputAlert("Template not saved!", "Error", ex.toString());
+			Statusbar.outputAlert("Template not saved!", "Error", ex.toString());
 			return false;
-		}		
+		}
 	}
-		
+
 	public void setServiceLayer(ServiceLayer serviceLayer) {
 		this.serviceLayer = serviceLayer;
 	}
-	
+
 	public void setDaoLayer(DAOLayer daoLayer) {
 		this.daoLayer = daoLayer;
 	}
