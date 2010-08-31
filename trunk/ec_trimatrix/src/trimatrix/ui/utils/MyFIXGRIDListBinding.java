d5d887b802492496f0d451f2de3278694aa508c2
reich.markus <reich.markus@2258e520-3029-11de-a0ce-9586e7784243>
2010-08-31 06:36:15 +0000

    New CC Version New on demand loading logic

--- trunk/ec_trimatrix/src/trimatrix/ui/utils/MyFIXGRIDListBinding.java
+++ trunk/ec_trimatrix/src/trimatrix/ui/utils/MyFIXGRIDListBinding.java
@@ -19,18 +19,33 @@ public class MyFIXGRIDListBinding<T extends FIXGRIDItem> extends FIXGRIDListBind
 		this.callback = callback;
 	}
 
-	public List<T> getRows() {
-		List<T> result = super.getRows();
-		// check if all items are read
-		boolean foundNullItem = false;
-		for (int i = 0; i < result.size(); i++) {
-			if (result.get(i) == null) {
-				foundNullItem = true;
-				int gridIndex = getSbvalue() + i;
-				T item = callback.createItem(gridIndex);
-				getItems().set(gridIndex, item);
-			}
-		}
-		return foundNullItem ? super.getRows() : result;
-	}
+    //Old, still valid way of load on demand - overriding the getRows() method
+    //Disadvantage: in getRows() you must not change the size of the item list (getItems()).
+//	@Override
+//	public List<T> getRows() {
+//		List<T> result = super.getRows();
+//		// check if all items are read
+//		boolean foundNullItem = false;
+//		for (int i = 0; i < result.size(); i++) {
+//			if (result.get(i) == null) {
+//				foundNullItem = true;
+//				int gridIndex = getSbvalue() + i;
+//				T item = callback.createItem(gridIndex);
+//				getItems().set(gridIndex, item);
+//			}
+//		}
+//		return foundNullItem ? super.getRows() : result;
+//	}
+
+	// "New" way of load on demand: executing load on demand when grid rendering starts.
+    @Override
+    public void renderingBegins() {
+        super.renderingBegins();
+        List<T> result = super.getRows();
+        // check if all items that are transferred to the client are read
+        for (int i=0; i<result.size(); i++) {
+            if (result.get(i) == null)
+            	callback.createItem(getSbvalue()+i);
+        }
+    }
 }
